8ab0191646918a4191eef13ee9b92cc701221da3
Araceli28 <98657580+Araceli28@users.noreply.github.com>
2022-12-13 22:52:31 -0600

    Add files via upload

---
+++ hospedajeScript.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO hoteldb.hospedaje VALUES (1,'PACIFICO');
+INSERT INTO hoteldb.hospedaje VALUES (2,'ALOHA');
+INSERT INTO hoteldb.hospedaje VALUES (3,'PRIMAVERA');
+SELECT * FROM hoteldb.hospedaje;
